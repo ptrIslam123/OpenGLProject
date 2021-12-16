d29281bddf889299c19c631b350ec1071692d026
Islam <i.kardanov@wisetech.pro>
2021-12-16 20:16:46 +0300

    Рисуется простой треугольник, его вершины разных цветов

--- main.cpp
+++ main.cpp
@@ -1,31 +1,28 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
  
+#include <exception> 
 #include <iostream>
- 
+#include <string>
+#include <array>
+#include <fstream>
+#include <sstream>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
- 
-// Константы
+
+std::string readShaderCode(const std::string &shaderSourcePath);
+
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
  
 int main()
 {
-    // glfw: инициализация и конфигурирование
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
  
-// Раскомментируйте данную часть кода, если используете macOS
-/*
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-*/
- 
-    // glfw: создание окна
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com", NULL, NULL);
     if (window == NULL)
     {
@@ -36,44 +33,144 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
  
-    // glad: загрузка всех указателей на OpenGL-функции
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }    
+
+    /** Создание шейдерной программы **/
+    unsigned int shader = 0;
+    unsigned int vertexShader = 0;
+    unsigned int fragmentShader = 0;
+
+    const std::string vertexShaderFileName = "../shaders/vertex.shader";
+    const std::string fragmentShaderFileName = "../shaders/fragment.shader";
+
+    std::string vertexSourceCode = readShaderCode(vertexShaderFileName);
+    std::string fragmentSourceCode = readShaderCode(fragmentShaderFileName);
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    int isSuccess = 1;
+    std::array<char, 512> infoLog = {};
+
+    const char *const vertexCode = vertexSourceCode.c_str();
+    const char *const fragmentCode = fragmentSourceCode.c_str();
+
+    glShaderSource(vertexShader, 1, &vertexCode, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isSuccess);
+
+    if (!isSuccess) {
+        glGetShaderInfoLog(vertexShader, infoLog.size(), NULL, infoLog.data());
+        std::cout << "Compile error in a vertext shader: " << infoLog.data() << std::endl;
+        return -1;
+    }
+
+
+    glShaderSource(fragmentShader, 1, &fragmentCode, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,  &isSuccess);
+
+    if (!isSuccess) {
+        glGetShaderInfoLog(fragmentShader, infoLog.size(), NULL, infoLog.data());
+        std::cout << "Compile error in a fragment shader: " << infoLog.data() << std::endl;
+        return -1;
+    }
+
+    shader = glCreateProgram();
+    glAttachShader(shader, vertexShader);
+    glAttachShader(shader, fragmentShader);
+    glLinkProgram(shader);
+    glGetShaderiv(shader,GL_LINK_STATUS,  &isSuccess);
+
+    if (!isSuccess) {
+        glGetShaderInfoLog(shader, infoLog.size(), NULL, infoLog.data());
+        std::cout << "Link error in a shader program: " << infoLog.data() << std::endl;
+        return -1;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    /** Шейдерная программа создана **/
+
+
+    /** Генерация буфера на GPU и заполнения буфера данными о вершинах **/
+    unsigned int vertexBufferObject = 0;
+    unsigned int colorBufferObject = 0;
+    unsigned int colorArrayObject = 0;
+    unsigned int vertexArrayObject = 0;
+    
+    float vertexes[] = {
+        0.5f, 1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f
+    };
+
+    float colors[] = {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STREAM_DRAW);
+
+    glGenBuffers(1, &colorBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STREAM_DRAW);
+
+    glGenVertexArrays(1, &vertexArrayObject);
+    glBindVertexArray(vertexArrayObject);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
  
-    // Цикл рендеринга
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBufferObject);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     while (!glfwWindowShouldClose(window))
     {
-        // Обработка ввода
         processInput(window);
 		
-		// Выполнение рендеринга
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
  
-        // glfw: обмен содержимым front- и back-буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
+        glUseProgram(shader);
+        glBindVertexArray(vertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
  
-    // glfw: завершение, освобождение всех ранее задействованных GLFW-ресурсов
     glfwTerminate();
     return 0;
 }
  
-// Обработка всех событий ввода: запрос GLFW о нажатии/отпускании клавиш на клавиатуре в данном кадре и соответствующая обработка данных событий
+std::string readShaderCode(const std::string &shaderSourcePath) {
+    std::ifstream file(shaderSourcePath);
+
+    if (!file.is_open()) {
+        throw std::runtime_error(shaderSourcePath + ": file not fount");
+    }
+    std::stringstream streamData;
+    streamData << file.rdbuf();
+    return streamData.str();
+}
+
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
  
-// glfw: всякий раз, когда изменяются размеры окна (пользователем или операционной системой), вызывается данная callback-функция
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // Убеждаемся, что окно просмотра соответствует новым размерам окна. 
-	// Обратите внимание, высота окна на Retina-дисплеях будет значительно больше, чем указано в программе
     glViewport(0, 0, width, height);
 }
