25aa5cc8924fadc2b47f554ab4967de652f7741b
Islam <i.kardanov@wisetech.pro>
2022-01-21 15:13:01 +0300

    немного поменял угля вращения

--- main.cpp
+++ main.cpp
@@ -169,7 +169,7 @@ int main()
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 proj = glm::mat4(1.0f);
 
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f)) * 
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 1.0f, 0.0f)) * 
                 glm::scale(model, glm::vec3(0.25f, 0.25f, 0.25f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         proj = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
