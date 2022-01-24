aa105dfe45c36e4d93766dfe2cd8e599f9999d2d
Islam <i.kardanov@wisetech.pro>
2022-01-24 19:01:51 +0300

    Добавил обработчик для получения данных о положений курсора мыши

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "glm/vec3.hpp"
 #include "glm/vec4.hpp"
 #include "glm/mat3x3.hpp"
@@ -20,7 +22,8 @@
 #include <sstream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow* window);
+void mouse_pos_callback(GLFWwindow* window, double xPos, double yPos);
 
 std::string readShaderCode(const std::string &shaderSourcePath);
 
@@ -30,7 +33,18 @@ void rotateVertices(float* vertices, unsigned int verticesSize, float rotateVal)
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+double curMouseXPos = 0.;
+double curMouseYPos = 0.;
  
+
+// Получения базисных векторов системы координат камеры
+glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f,  0.0f);
+
+float deltaTime = 0.0f;	// время между текущим кадром и последним кадром
+float lastFrame = 0.0f;
+
 int main()
 {
     glfwInit();
@@ -47,6 +61,11 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // Установить функция-колбэк для получения данных о позиции курсора мыши(тачпада)
+    glfwSetCursorPosCallback(window, mouse_pos_callback);
+
+    // Сообщаем GLFW, чтобы он захватил наш курсор (Делает просто не видимым для пользователя!)
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
  
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -113,83 +132,121 @@ int main()
 
 
     /** Генерация буфера на GPU и заполнения буфера данными о вершинах **/
+    unsigned int modelLocation = 0;
+    unsigned int viewLocation = 0;
+    unsigned int projLocation = 0;
     unsigned int vertexBufferObject = 0;
-    unsigned int vertexArrayObject = 0;
+    unsigned int elementBufferObject = 0;
     unsigned int colorBufferObject = 0;
-    
+    unsigned int colorArrayObject = 0;
+    unsigned int vertexArrayObject = 0;
     unsigned int vertexSize = 3;
     unsigned int colorSize = 3;
+    unsigned int indexSize = 3;
+    unsigned int vertexCounter = 36;
 
 
-    // Указание вершин (и буфера(ов)) и настройка вершинных атрибутов
-	float vertices[] = {
-		  // координаты        // текстурные координаты
-		 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		  0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
+    // вершины куба (6 граней * 2 треугольника * 3 вершины каждый)
+    float vertices[] = {
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+ 
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+ 
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+ 
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+ 
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f, -0.5f,
+ 
+    -0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f
+    };
     
+
+    glGenBuffers(1, &vertexBufferObject);
+    glGenBuffers(1, &elementBufferObject);
+    glGenVertexArrays(1, &vertexArrayObject);
+
+    glBindVertexArray(vertexArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, vertexSize, GL_FLOAT, GL_FALSE, sizeof(float) * (vertexSize), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
+        // Логическая часть работы со временем для каждого кадра
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
         processInput(window);
-		
-        /** Последовательное применение вращения, масштабирования и сдвига треугольника **/
-        rotateVertices(vertices, sizeof(vertices), 50.f);
-        scaleVertices(vertices, sizeof(vertices), 1.00025f);
-        translateVertices(vertices, sizeof(vertices), 2.5f);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
  
         glUseProgram(shader);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
 
-        glBindBuffer(GL_ARRAY_BUFFER, colorBufferObject);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+        model = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)) * 
+                glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 0.5f, 0.0f)) * 
+                glm::scale(model, glm::vec3(0.75f, 0.75f, 0.75f));
+
+        view = glm::lookAt(
+            cameraPosition,                             // вектор позиции камеры 
+            cameraPosition + cameraDirection,           // вектор цели камеры
+            cameraUp                                    // вектор вверх камеры
+        );
+
+        proj = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        
+        modelLocation = glGetUniformLocation(shader, "model");
+        viewLocation = glGetUniformLocation(shader, "view");
+        projLocation = glGetUniformLocation(shader, "proj");
+
+        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLocation, 1, GL_FALSE, glm::value_ptr(proj));
 
         glBindVertexArray(vertexArrayObject);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, vertexCounter);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -197,62 +254,6 @@ int main()
  
     glfwTerminate();
     return 0;
-}
- 
-
-void rotateVertices(float* vertices, unsigned int verticesSize, const float rotateVal) {
-    /** Матрица вращения по оси Z **/
-    const glm::mat4 rotate(
-        cos(rotateVal), -sin(rotateVal), 0.0f, 0.0f,
-        sin(rotateVal), cos(rotateVal), 0.0f, 0.0f,
-        0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.f, 0.0f, 1.0f
-    );
-
-    for (int i = 0; i < verticesSize; ++i) {
-        const glm::vec4 vertex(vertices[i], vertices[i + 1], vertices[i + 2], 1.0f);
-        const glm::vec4 delVertex = rotate * vertex;
-
-        vertices[i] = delVertex.x;
-        vertices[i + 1] = delVertex.y;
-        vertices[i + 2] = delVertex.z;
-    }
-}
-
-void scaleVertices(float* vertices, unsigned int verticesSize, const float scaleVal) {
-    const glm::mat3 scale(
-        scaleVal, 0.0f, 0.0f,
-        0.0f, scaleVal, 0.0f,
-        0.0f, 0.0f, scaleVal
-    );
-
-    for (int i = 0; i < verticesSize; ++i) {
-        const glm::vec3 vertex(vertices[i], vertices[i + 1], vertices[i + 2]);
-        const glm::vec3 delVertex = scale * vertex;
-
-        vertices[i] = delVertex.x;
-        vertices[i + 1] = delVertex.y;
-        vertices[i + 2] = delVertex.z;
-    }
-}
-
-
-void translateVertices(float* vertices, unsigned int verticesSize, const float translateVal) {
-    const glm::mat4 translate(
-        1.0f, 0.0f, 0.0f, translateVal,
-        0.0f, 1.0f, 0.0f, translateVal,
-        0.0f, 0.0f, 1.0f, translateVal,
-        0.0f, 0.0f, 0.0f, 1.0f
-    );
-
-    for (int i = 0; i < 1; ++i) {
-        const glm::vec4 vertex(vertices[i], vertices[i + 1], vertices[i + 2], 1.0f);
-        const glm::vec4 delVertex = translate * vertex;
-
-        vertices[i] = delVertex.x;
-        vertices[i + 1] = delVertex.y;
-        vertices[i + 2] = delVertex.z;
-    }
 }
 
 std::string readShaderCode(const std::string &shaderSourcePath) {
@@ -264,6 +265,14 @@ std::string readShaderCode(const std::string &shaderSourcePath) {
     std::stringstream streamData;
     streamData << file.rdbuf();
     return streamData.str();
+}
+
+void mouse_pos_callback(GLFWwindow* window, const double xPos, const double yPos) {
+    std::cout << "xPos := " << curMouseXPos << "\tyPos := " << curMouseYPos << std::endl;
+    curMouseXPos = xPos;
+    curMouseYPos = yPos;
+    std::cout << "xPos` := " << curMouseXPos << "\tyPos` := " << curMouseYPos << std::endl;
+    std::cout << std::endl;
 }
 
 void processInput(GLFWwindow *window)
--- shaders/vertex.shader
+++ shaders/vertex.shader
@@ -1,11 +1,14 @@
 #version 330 core
 
 layout(location = 0) in vec3 vertexPos;
-layout(location = 1) in vec3 vertexColor;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
 out vec3 Color;
 
 void main() {
-    gl_Position = vec4(vertexPos, 1.0);
-    Color = vertexColor;
+    gl_Position = proj * view * model * vec4(vertexPos, 1.0);
+    Color = vec3(1.0, 0.0, 1.0);
 }
