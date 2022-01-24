7e185841ab22d1fcaad8635c56a47105340ed4e0
Islam <i.kardanov@wisetech.pro>
2022-01-24 12:18:43 +0300

    нормально работающая матрица lookAt

--- main.cpp
+++ main.cpp
@@ -38,6 +38,8 @@ glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
+
+
 float deltaTime = 0.0f;	// время между текущим кадром и последним кадром
 float lastFrame = 0.0f;
 
